8f078be223f9f59f663f4582192fb63a937a2fc7
Jim Galloway <jwrg@uvic.ca>
2020-05-05 12:19:11 -0700

    Update database schema
    Small update to the schema, whereby the name is changed to
    differentiate the database from the user, and so that
    applicable privileges and ownership are doled out.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,5 @@
+create user kcats with password 'stackbackwards';
+create database kcatsdb owner kcats;
+\c kcatsdb;
 create table json (id integer not null,date timestamp,data jsonb,primary key (id) );
+grant all privileges on json to kcats;
