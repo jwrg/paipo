195f581abad499124a50867993ee553824547429
Jim Galloway <jwrg@uvic.ca>
2020-05-07 17:40:03 -0700

    Update calendar view to display dummy data
    The calendar view has been adapted from an older app to
    visualize flavourless json fetched from the database.  The
    database query for this view only asks for the days which
    have datapoints associated with them for the current month,
    with no regard for how many datapoints each day has, or
    what kind of data is associated with said day.
    
    This month view is wrapped in a div which can be made
    smaller, so pending some changes should serve well as a
    basis for a year view, which will be accessed using the
    calendar view, only giving one parameter (the year)
    instead of two (year, month).

--- db/queries.js
+++ db/queries.js
@@ -12,5 +12,14 @@ const queries = module.exports = {
     past: {
       text: 'SELECT * FROM json WHERE date < now() ORDER BY date DESC LIMIT 5'
     }
+  },
+  calendar: {
+    dates : (year, month) => {
+      return {
+        text: "SELECT DISTINCT EXTRACT(DAY FROM date) FROM json WHERE " +
+          "date >= '" + year + "-" + (Math.floor(parseInt(month)/10) ? (parseInt(month) + 1) : "0" + (parseInt(month) + 1)) + "-01' AND " +
+          "date <= '" + year + "-" + (Math.floor(parseInt(month)/10) ? (parseInt(month) + 2) : "0" + (parseInt(month) + 2)) + "-01'"
+      };
+    }
   }
 }
