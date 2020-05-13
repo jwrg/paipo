28d20970c74e7f01980670bf81759d7c28988619
Jim Galloway <jwrg@uvic.ca>
2020-05-13 12:56:02 -0700

    Clean up and add to dummy data
    Some more dummy data has been added to help shore up some
    edge cases wrt getting those nice corridors to display on
    the edit view.  Furthermore, the sql file looks nicer and
    follows some basic JSON conventions.

--- sql/dummy.sql
+++ sql/dummy.sql
@@ -1,9 +1,68 @@
 INSERT INTO json (date, data) VALUES 
-( '2020-05-25 19:50:50', 
-  '{"name": "Person", "age": "Ninety-nine"}' ),
-( '2020-05-26 20:20:20',
-  '{"name": "Person 2", "gender": "Szchklymale"}'),
-( '2020-05-26 21:20:20',
-  '{"name": "Person 3", "gender": "Cisgendered "}'),
-( '2020-05-28 22:20:20',
-  '{"name": "Ric", "nickname": "Nature Boy", "catchphrase": "WOOOOOOOOO"}')
+( 
+  '2020-05-25 19:50:50', 
+ '{
+    "name": "Person", 
+    "age": "Ninety-nine"
+  }' 
+),
+( 
+  '2020-05-26 20:20:20',
+ '{
+    "name": "Person 2", 
+    "gender": "Szchklimayl"
+  }'
+),
+( 
+  '2020-05-26 21:20:20',
+ '{
+    "name": "Person 3", 
+    "schtick": "Ventriloquism"
+  }'
+),
+( 
+  '2020-05-28 22:20:20',  
+ '{
+    "name": "Ric", 
+    "nickname": "Nature Boy", 
+    "catchphrase": "WOOOOOOOOO"
+  }'
+),
+( 
+  '2020-05-30 12:10:10',
+ '{
+    "name": "Kuro-Chan", 
+    "Eats": 
+    {
+      "Favourite": "Chikin",
+      "Second favourite": "Italian food",
+      "Daisuki desu": "Rice double portion",
+      "Kinda unpalatable": "Vegetables",
+      "Kinda good everywhere": "Frog legs"
+    },
+    "Coming soon": "To a concealed pit near you"
+  }'
+),
+( 
+  '2020-05-29 08:29:29',
+ '{
+    "name": "Ricky", 
+    "Professions":
+    {
+      "Most of the time": "Growing dope",
+      "Some of the time":
+      {
+        "Vocational School":
+        {
+          "During school hours": "Custodial arts",
+          "After hours": "Leave hash in the lockers",
+          "Anyone asks": "Different things"
+        },
+        "Stealing":[ "Meat", "Barbecues", "Groceries", "Lawn mowers", "Wheels" ],
+        "Banging": "Lucy",
+        "Otherwise": "I can''t think of anything else but I am smart"
+      }
+    }
+  }'
+)
+;
