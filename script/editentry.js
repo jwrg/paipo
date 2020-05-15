4a6c8449c887493506ea297b4dbe087cbe2ffbf2
Jim Galloway <jwrg@uvic.ca>
2020-05-14 20:49:13 -0700

    Implement expandField function for JSON editor
    The JSON editor now allows for turning a data field into
    one or more sub-fields.  Much code was moved around to
    adhere to the DRY principle, and some classes and id's are
    now a bit more consistent in the edit view template.

--- script/editentry.js
+++ script/editentry.js
@@ -1,67 +1,99 @@
-function addField(caller) {
-  /* Extract number from caller's parent's id */
-  let parentDiv;
-  let number;
-  if (caller) {
-    parentDiv = caller.parentNode.parentNode;
-    number = parentDiv.lastElementChild.id.substring(9);
-  } else {
-    parentDiv = document.getElementById('datafields');
-    number = parentDiv.lastElementChild.id.substring(9);
-  }
+function extractIdentifier(node) {
+  return node.parentNode.parentNode.id.substring(9);
+}
 
+function nextIdentifier(node) {
+  return incrementIdentifier(node.parentNode.parentNode.lastElementChild.id.substring(9));
+}
+
+function incrementIdentifier(str) {
   /* Split number into array of constituent levels */
-  let numbers = number.toString().split('_');
+  let numbers = str.toString().split('_');
+  /* Pop last number, increment and push it back */
   let lastNumber = numbers.pop();
   numbers.push(++lastNumber);
-  let newNumber = numbers.join('_');
+  /* Make a new string to use in id attributes */
+  return numbers.join('_');
+} 
 
+function decrementIdentifier(str) {
+  /* Split number into array of constituent levels */
+  let numbers = str.toString().split('_');
+  /* Pop last number, increment and push it back */
+  let lastNumber = numbers.pop();
+  numbers.push(--lastNumber);
+  /* Make a new string to use in id attributes */
+  return numbers.join('_');
+}
+
+function createEditorButton(onclick) {
+  let words = [];
+  if (onclick === 'addField') {
+    words = ['add','Field','Add','a subfield','∃'];
+  }
+  if (onclick === 'expandField') {
+    words = ['expand','Field','Expand','this field','↳'];
+  }
+  if (onclick === 'deleteField') {
+    words = ['delete','Field','Delete','this field','⌫'];
+  }
+  if (onclick === 'collapseLevel') {
+    words = ['collapse','Level','Collapse','this level','↰'];
+  }
+  if (onclick === 'deleteLevel') {
+    words = ['delete','Level','Delete','this level','⌫'];
+  }
+  let newButton = document.createElement('input');
+  newButton.setAttribute('class','button');
+  newButton.setAttribute('type','button');
+  newButton.setAttribute('onclick',words[0] + words[1] + '(this)');
+  newButton.setAttribute('aria-label',words[2] + ' ' + words[3]);
+  newButton.setAttribute('title',words[2] + ' ' + words[3]);
+  newButton.setAttribute('value',words[4]);
+  return newButton;
+}
+
+function createDatatier(number, key = '', value = '') {
   /* Create datatier div to encase KV pair */
   let newTier = document.createElement('div');
-  newTier.setAttribute('id', 'datatier_' + newNumber);
+  newTier.setAttribute('id', 'datatier_' + number);
   newTier.setAttribute('class', 'datatier');
-
-  /* Create corridor p and adjust immediately prior */
-  let oldCorridor = parentDiv.lastElementChild.firstElementChild;
-  let newCorridor = oldCorridor.cloneNode(true);
-  oldCorridor.lastElementChild.textContent = '├─';
-
 
   /* Create fieldkey div for key label/input pair */
   let newKeyDiv = document.createElement('div');
-  newKeyDiv.setAttribute('id', 'field_' + newNumber + '_key');
+  newKeyDiv.setAttribute('id', 'field_' + number + '_key');
   newKeyDiv.setAttribute('class', 'fieldkey');
 
   /* Create label/input pair and append to fieldkey div */
   let newKeyLabel = document.createElement('label');
   newKeyLabel.setAttribute('class', 'fieldkey');
-  newKeyLabel.setAttribute('for', 'field_' + newNumber + '_key');
-  newKeyLabel.appendChild(document.createTextNode('Data Field ' + numbers.join(', ') + ' Key:'));
+  newKeyLabel.setAttribute('for', 'field_' + number + '_key');
+  newKeyLabel.appendChild(document.createTextNode('Data Field ' + number.split('_').join(', ') + ' Key:'));
   let newKeyInput = document.createElement('input');
-  newKeyInput.setAttribute('id', 'field_' + newNumber + '_key_input');
+  newKeyInput.setAttribute('id', 'field_' + number + '_key_input');
   newKeyInput.setAttribute('class', 'fieldkey');
   newKeyInput.setAttribute('type', 'text');
-  newKeyInput.setAttribute('name', 'field_' + newNumber + '_key');
-  newKeyInput.setAttribute('value', '');
+  newKeyInput.setAttribute('name', 'field_' + number + '_key');
+  newKeyInput.setAttribute('value', key);
   newKeyDiv.appendChild(newKeyLabel);
   newKeyDiv.appendChild(newKeyInput);
 
   /* Create fieldvalue div for value label/input pair */
   let newValueDiv = document.createElement('div');
-  newValueDiv.setAttribute('id', 'field_' + newNumber + '_value');
+  newValueDiv.setAttribute('id', 'field_' + number + '_value');
   newValueDiv.setAttribute('class', 'fieldvalue');
 
   /* Create label/input pair and append to fieldvalue div */
   let newValueLabel = document.createElement('label');
   newValueLabel.setAttribute('class', 'fieldvalue');
-  newValueLabel.setAttribute('for', 'field_' + newNumber + '_value');
-  newValueLabel.appendChild(document.createTextNode('Data Field ' + numbers.join(', ') + ' Value:'));
+  newValueLabel.setAttribute('for', 'field_' + number + '_value');
+  newValueLabel.appendChild(document.createTextNode('Data Field ' + number.split('_').join(', ') + ' Value:'));
   let newValueInput = document.createElement('input');
-  newValueInput.setAttribute('id', 'field_' + newNumber + '_value_input');
+  newValueInput.setAttribute('id', 'field_' + number + '_value_input');
   newValueInput.setAttribute('class', 'fieldvalue');
   newValueInput.setAttribute('type', 'text');
-  newValueInput.setAttribute('name', 'field_' + newNumber + '_value');
-  newValueInput.setAttribute('value', '');
+  newValueInput.setAttribute('name', 'field_' + number + '_value');
+  newValueInput.setAttribute('value', value);
   newValueDiv.appendChild(newValueLabel);
   newValueDiv.appendChild(newValueInput);
   
@@ -85,14 +117,94 @@ function addField(caller) {
   newValueDiv.appendChild(newDeleteButton);
 
   /* Append all to datatier */
-  newTier.appendChild(newCorridor);
   newTier.appendChild(newKeyDiv);
   newTier.appendChild(newValueDiv);
+
+  return newTier;
+}
+
+function addField(caller) {
+  /* Extract number from caller's parent's id */
+  let parentDiv;
+  let number;
+  if (caller) {
+    parentDiv = caller.parentNode.parentNode;
+    number = nextIdentifier(caller);
+  } else {
+    parentDiv = document.getElementById('datafields');
+    number = (parseInt(parentDiv.lastElementChild.id.substring(9)) + 1).toString();
+  }
+
+  /* Create a new datatier with empty key and value */
+  let newTier = createDatatier(number);
+
+  /* Create corridor p and adjust immediately prior */
+  let oldCorridor = parentDiv.lastElementChild.firstElementChild;
+  let newCorridor = oldCorridor.cloneNode(true);
+  if (oldCorridor.lastElementChild.textContent == '└┬') {
+    newCorridor.lastElementChild.textContent = '└─';
+    parentDiv.lastElementChild.lastElementChild.firstElementChild.firstElementChild.textContent = '│';
+    let subject = parentDiv.lastElementChild.lastElementChild;
+    while (subject.firstElementChild.firstElementChild) {
+      subject.firstElementChild.firstElementChild.textContent = '│';
+      subject = subject.lastElementChild;
+    }
+    oldCorridor.lastElementChild.textContent = '├┬';
+  } else {
+    oldCorridor.lastElementChild.textContent = '├─';
+  }
+  newTier.prepend(newCorridor);
 
   /* Append datatier to new parent */
   parentDiv.appendChild(newTier);
 }
-function expandField() {}
+
+function expandField(caller) {
+  /* Extract number from caller's parent id */
+  let number = extractIdentifier(caller);
+
+  /* Get current field's value for use as new field's key */
+  let value = document.getElementById('field_' + number + '_value_input').value;
+
+  /* Create datatier div to encase new KV pair */
+  let newTier = createDatatier(number + '_1', value, '');
+
+  /* Duplicate and adjust parent's corridor */
+  let topCorridor = caller.parentNode.parentNode.firstElementChild;
+  let newCorridor = topCorridor.cloneNode(true);
+  /* New corridor is contingent on old one */
+  if (topCorridor.lastElementChild.textContent == '└─') {
+    newCorridor.lastElementChild.textContent = '\xa0';
+  } else {
+    newCorridor.lastElementChild.textContent = '│';
+  }
+  let newPipe = document.createElement('tt');
+  newPipe.appendChild(document.createTextNode('└─'));
+  newCorridor.appendChild(newPipe);
+  newTier.prepend(newCorridor);
+
+  /* Change parent corridor to branch down */
+  topCorridor.lastElementChild.textContent = topCorridor.lastElementChild.textContent.slice(0,1) + '┬';
+
+
+  /* Create buttons for key div and append */
+  let newAddButton = createEditorButton('addField');
+  let newCollapseButton = createEditorButton('collapseLevel');
+  let newDeleteButton = createEditorButton('deleteLevel');
+  caller.parentNode.appendChild(newAddButton);
+  caller.parentNode.appendChild(newCollapseButton);
+  caller.parentNode.appendChild(newDeleteButton);
+  /* Change old value to be new sub-key */
+  /* Append new subtier */
+  caller.parentNode.parentNode.appendChild(newTier);
+
+  /* Delete old value div (and children) */
+  caller.nextElementSibling.remove();
+  caller.previousElementSibling.remove();
+  caller.previousElementSibling.remove();
+  caller.remove();
+}
+
 function deleteField() {}
 function collapseLevel() {}
 function deleteLevel() {}
