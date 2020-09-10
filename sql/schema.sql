cede0d63fbe325099189998f61e987b38afcd384
Jim Galloway <jwrg@uvic.ca>
2020-09-10 11:42:11 -0700

    Update some files to reflect project name change
    Some files containing the project's preliminary name have
    been adjusted to have the correct name.  Hopefully I don't
    get an inkling to change the name again.

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,10 @@
-CREATE USER kcats WITH PASSWORD 'stackbackwards';
-CREATE DATABASE kcatsdb OWNER kcats;
-\c kcatsdb;
+CREATE USER paipo WITH PASSWORD 'boardbackwards';
+CREATE DATABASE paipodb OWNER paipo;
+\c paipodb;
 CREATE TABLE json (
   id SERIAL PRIMARY KEY,
   date TIMESTAMP,
   data JSONB
 );
-GRANT ALL PRIVILEGES ON json TO kcats;
-GRANT USAGE, SELECT ON SEQUENCE json_id_seq TO kcats;
+GRANT ALL PRIVILEGES ON json TO paipo;
+GRANT USAGE, SELECT ON SEQUENCE json_id_seq TO paipo;
